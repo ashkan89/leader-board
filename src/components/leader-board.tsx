cdaab58c244a7f570997197520ad7ab18c36613b
Ashkan Shirian <ashkan.shirian@live.com>
2023-08-13 17:43:51 +0330

    infinity scroll client side

--- src/components/leader-board.tsx
+++ src/components/leader-board.tsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useRef, useState } from 'react'
 
 function LeaderBoard(props: any) {
+    const [items, setItems] = useState<any>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<any>(null);
+    const page = useRef(0);
+    const finishLoading = useRef(false);
+    const listRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const handleScroll = () => {
+        if (listRef?.current?.clientHeight! + listRef?.current?.scrollTop! !== listRef?.current?.scrollHeight || isLoading) {
+            return;
+        }
+
+        fetchData();
+    };
+
+    useEffect(() => {
+        listRef?.current?.addEventListener('scroll', handleScroll);
+        return () => listRef?.current?.removeEventListener('scroll', handleScroll);
+    }, [isLoading]);
+
+    const fetchData = () => {
+        if (finishLoading.current) {
+            return;
+        }
+
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const pageSize = props.boardItems.length - page.current * props.pageSize < props.pageSize ? props.boardItems.length - page.current * props.pageSize : props.pageSize;
+
+            if (pageSize < props.pageSize) {
+                finishLoading.current = true;
+            }
+
+            const data = props.boardItems.slice(page.current * props.pageSize, page.current * props.pageSize + pageSize);
+            setItems((prevItems: any) => [...prevItems, ...data]);
+            page.current++;
+        } catch (error: any) {
+            setError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
         <div className="container">
             <div className="topLeadersList">
-                {props.items.map((leader: any, index: number) => (
-                    <div className="leader" key={leader.id}>
+                {items.map((leader: any, index: number) => (
+                    <div className="leader" key={index}>
                         {index + 1 <= 3 && (
                             <div className="containerImage">
                                 <img className="image" loading="lazy" src={leader.image} alt='' />
@@ -29,6 +77,8 @@ function LeaderBoard(props: any) {
                         )}
                     </div>
                 ))}
+                {isLoading && <p>Loading...</p>}
+                {error && <p>Error: {error.message}</p>}
             </div>
 
             <div className="playerslist">
@@ -37,22 +87,14 @@ function LeaderBoard(props: any) {
                     <div>Name</div>
                     <div>LVL</div>
                     <div>XP</div>
-                    <div>
-                        Coins
-                    </div>
-                    <div>
-                        Likes
-                    </div>
-                    <div>
-                        Pass
-                    </div>
-                    <div>
-                        Resources
-                    </div>
+                    <div>Coins</div>
+                    <div>Likes</div>
+                    <div>Pass</div>
+                    <div>Resources</div>
                 </div>
-                <div className="list">
-                    {props.items.map((leader: any, index: number) => (
-                        <div className={`player ${index === 9 ? 'sticky' : ''}`} key={leader.id}>
+                <div className="list" ref={listRef}>
+                    {items.map((leader: any, index: number) => (
+                        <div className={`player ${index === 9 ? 'sticky' : ''}`} key={index}>
                             <span> {index + 1}</span>
                             <div className="user">
                                 <img className="image" src={leader.image} alt='' />
@@ -70,10 +112,6 @@ function LeaderBoard(props: any) {
             </div>
         </div>
     )
-}
-
-LeaderBoard.propTypes = {
-    items: []
 }
 
 export default LeaderBoard
